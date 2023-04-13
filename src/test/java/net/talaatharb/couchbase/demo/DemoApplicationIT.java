6c60f8d2ef4aa221e265b1800d4b6216b97c489d
mharb <mohamed.harb@cegedim.com>
2023-04-13 14:30:55 +0200

    added assert for context loading IT

--- src/test/java/net/talaatharb/couchbase/demo/DemoApplicationIT.java
+++ src/test/java/net/talaatharb/couchbase/demo/DemoApplicationIT.java
@@ -1,13 +1,21 @@
 package net.talaatharb.couchbase.demo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DemoApplicationIT {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext);
 	}
 
 }
